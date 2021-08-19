cb0fd40047e787c32ce6af1d511fa4c85d0bb4c7
Sean Morrison <brlcad@gmail.com>
2021-08-19 08:01:49 -0700

    header guards and c symbols for c++

--- progpath.h
+++ progpath.h
@@ -1,4 +1,16 @@
+#ifndef PROGPATH_H
+#define PROGPATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stddef.h>
 
 extern char *progpath(char *buf, size_t len);
 
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PROGPATH_H */
