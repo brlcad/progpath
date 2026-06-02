f843a4c197a517d573f36030be23bfcb21e27482
Christopher Sean Morrison <brlcad@gmail.com>
2026-06-02 00:00:14 -0400

    cleanup indent and make ipwd check failure return an error

--- tests/test_c_init.c
+++ tests/test_c_init.c
@@ -18,32 +18,32 @@
 #endif
 
 int main(void) {
-    char initial_expected[MAXPATHLEN] = {0};
-    char *ipwd;
+  char initial_expected[MAXPATHLEN] = {0};
+  char *ipwd;
 
-    if (!getcwd(initial_expected, sizeof(initial_expected))) {
-        fprintf(stderr, "getcwd failed\n");
-        return 1;
-    }
+  if (!getcwd(initial_expected, sizeof(initial_expected))) {
+    fprintf(stderr, "getcwd failed\n");
+    return 1;
+  }
 
-    if (chdir("..") != 0) {
-        fprintf(stderr, "chdir failed\n");
-        return 1;
-    }
+  if (chdir("..") != 0) {
+    fprintf(stderr, "chdir failed\n");
+    return 1;
+  }
 
-    ipwd = progipwd(NULL, 0);
-    if (!ipwd) {
-        fprintf(stderr, "progipwd failed\n");
-        return 1;
-    }
+  ipwd = progipwd(NULL, 0);
+  if (!ipwd) {
+    fprintf(stderr, "progipwd failed\n");
+    return 1;
+  }
 
-    if (strcmp(initial_expected, ipwd) != 0) {
-        fprintf(stderr, "FAIL: Expected ipwd '%s', but got '%s'\n", initial_expected, ipwd);
-        free(ipwd);
-        return 1;
-    }
-
-    printf("PASS: ipwd matches '%s'\n", ipwd);
+  if (strcmp(initial_expected, ipwd) != 0) {
+    fprintf(stderr, "FAIL: Expected ipwd '%s', but got '%s'\n", initial_expected, ipwd);
     free(ipwd);
-    return 0;
+    return 1;
+  }
+
+  printf("PASS: ipwd matches '%s'\n", ipwd);
+  free(ipwd);
+  return 0;
 }
